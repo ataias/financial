dffe215263fc00300d33857f5f9174e506f26e05
Ataias Pereira Reis <ataiasreis@gmail.com>
2019-09-15 11:40:23 -0300

    Add Easyinvest CSV parser (#1)
    * Add initial files for easyinvest module
    * Convert dos2unix
    * Format using fantomas

--- Interest.Test/Tests.fs
+++ Interest.Test/Tests.fs
@@ -1,27 +1,72 @@
 module Tests
 
 open System
+open System.IO
 open Xunit
 open Interest
+open Interest.Interest
 
 let lowValue = -0.001
 let highValue = 0.001
 
+
 [<Fact>]
-let ``Test interest for one month without delta`` () =
+let ``Test interest for one month without delta``() =
     let rate = 0.1 / 100.0
     let amount = 10000.
     let delta = 0.
     let months = 1
-    let newAmount = Interest.interest rate amount delta months
+    let newAmount = interest rate amount delta months
     Assert.Equal(amount * (1. + rate) ** 1., newAmount)
 
 [<Fact>]
-let ``Test interest for 5 months without delta`` () =
+let ``Test interest for 5 months without delta``() =
     let rate = 0.1 / 100.0
     let amount = 10000.
     let delta = 0.
     let months = 5
-    let newAmount = Interest.interest rate amount delta months
+    let newAmount = interest rate amount delta months
     // comparison of doubles is tricky, so accepting range here
     Assert.InRange(amount * (1. + rate) ** 5. - newAmount, lowValue, highValue)
+
+[<Fact>]
+let ``Import EasyInvest CSV data into a format parsable later``() =
+    let parsed =
+        read EasyInvest "../../../../data/easyinvest-2019-no-sep.csv"
+        |> Array.rev
+        |> Array.take 3
+
+    let (expected: InvestmentInfo []) =
+        [| { Broker = EasyInvest
+             Qty = 20.0M
+             Investment = "Ações"
+             Description = "ACTI01"
+             DueDate = None
+             AgreedRate = None
+             InitialAmount = None
+             GrossAmount = 2571.6
+             NetAmount = None }
+           { Broker = EasyInvest
+             Qty = 2.0M
+             Investment = "Tesouro Direto"
+             Description = "Tesouro 5"
+             DueDate = Some (DateTime(2035, 5, 15))
+             AgreedRate = Some "IPCA"
+             InitialAmount = Some 2615.3
+             GrossAmount = 3636.08
+             NetAmount = Some 3455.57 }
+           { Broker = EasyInvest
+             Qty = 1.10M
+             Investment = "Tesouro Direto"
+             Description = "Tesouro 4"
+             DueDate = Some (DateTime(2035, 5, 15))
+             AgreedRate = Some "IPCA"
+             InitialAmount = Some 3648.25
+             GrossAmount = 4616.28
+             NetAmount = Some 4444.49 } |]
+
+    Assert.Equal(expected.Length, parsed.Length)
+    // Calling Assert.Equal for each element allows us to see a more
+    // human-readable error message when the test fails
+    for (e, p) in Array.zip expected parsed do
+      Assert.Equal(e, p)
--- Interest/Library.fs
+++ Interest/Library.fs
@@ -1,57 +1,112 @@
-﻿namespace Interest
+namespace Interest
+
 open System
+open FSharp.Data
 
 module Interest =
-  type Header = {
-      Value: float
-      Symbol: string
-  }
 
-  let rec public interest rate amount delta n =
-      match n with
-          | 0 -> amount
-          | _ ->
-              interest rate (amount*(1.+rate) + delta) delta (n-1)
+    type Header =
+        { Value: float
+          Symbol: string }
+
+    type Broker = EasyInvest
+
+    type InvestmentInfo =
+        { Broker: Broker
+          Qty: decimal
+          Investment: string
+          Description: string
+          DueDate: DateTime option
+          AgreedRate: string option
+          InitialAmount: float option
+          GrossAmount: float
+          NetAmount: float option }
 
 
-  let public simulate nYears =
-      let rates = [for i in 1..15 -> 0.1*(float i)/100.0]
-      let n = rates.Length
-      let initialAmount = 120000.0
-      let delta = [for i in 10..50 -> float (i * 100)]
-      let m = delta.Length
-      let months = 12 * nYears
-      let getAmount i j =
-        let amount = interest rates.[i] initialAmount delta.[j] months
-        let nextInterest = amount * rates.[i]
-        (amount, nextInterest)
-      let matrix = Array2D.init n m getAmount
-      let topHeader = List.map (fun x -> { Value = x * 100.0; Symbol = "%" }) rates
-      let leftHeader = List.map (fun x -> { Value = x; Symbol = "R$" }) delta
-      (topHeader, leftHeader, matrix)
+    type EasyInvestTable = CsvProvider<"../data/easyinvest-2019-no-sep.csv", ";", PreferOptionals=true, Culture="pt-BR">
 
-  let public printArray (arr: string [,]): unit =
-      for line = 0 to Array2D.length1 arr - 1 do
-          if line = 1 then
-              printfn "|-"
-          for column = 0 to Array2D.length2 arr - 1 do
-              // printfn "(%d, %d)" line column
-              printf "| %s " arr.[line, column]
-          printfn "|"
+    let rec public interest rate amount delta n =
+        match n with
+        | 0 -> amount
+        | _ -> interest rate (amount * (1. + rate) + delta) delta (n - 1)
 
-  let public createTable title (data: float[,]) (top:Header list) (left: Header list) : string[,] =
-      let genElement i j =
-          match (i, j) with
-              | 0, j when j > 0 ->
-                  let h = top.[j-1]
-                  sprintf "%.2f%s" h.Value h.Symbol
-              | i, 0 when i > 0 ->
-                  let h = left.[i-1]
-                  sprintf "%s %.2f" h.Symbol h.Value
-              | i, j when i > 0 && j > 0 ->
-                  data.[j-1, i-1].ToString("#,##0.00")
 
-              | _, _ -> title
-      let n = left.Length + 1
-      let m = top.Length + 1
-      Array2D.init n m genElement
+    /// <summary>
+    /// Simulates evolution of capital for different interest rates and
+    /// different injections of money per month for a given amount of years
+    /// </summary>
+    let public simulate nYears =
+        let rates =
+            [ for i in 1 .. 15 -> 0.1 * (float i) / 100.0 ]
+
+        let n = rates.Length
+        let initialAmount = 120000.0
+
+        let delta =
+            [ for i in 10 .. 50 -> float (i * 100) ]
+
+        let m = delta.Length
+        let months = 12 * nYears
+
+        let getAmount i j =
+            let amount = interest rates.[i] initialAmount delta.[j] months
+            let nextInterest = amount * rates.[i]
+            (amount, nextInterest)
+
+        let matrix = Array2D.init n m getAmount
+
+        let topHeader =
+            List.map (fun x ->
+                { Value = x * 100.0
+                  Symbol = "%" }) rates
+
+        let leftHeader =
+            List.map (fun x ->
+                { Value = x
+                  Symbol = "R$" }) delta
+
+        (topHeader, leftHeader, matrix)
+
+    let public printArray (arr: string [,]): unit =
+        for line = 0 to Array2D.length1 arr - 1 do
+            if line = 1 then printfn "|-"
+            for column = 0 to Array2D.length2 arr - 1 do
+                // printfn "(%d, %d)" line column
+                printf "| %s " arr.[line, column]
+            printfn "|"
+
+    let public createTable title (data: float [,]) (top: Header list)
+        (left: Header list): string [,] =
+        let genElement i j =
+            match (i, j) with
+            | 0, j when j > 0 ->
+                let h = top.[j - 1]
+                sprintf "%.2f%s" h.Value h.Symbol
+            | i, 0 when i > 0 ->
+                let h = left.[i - 1]
+                sprintf "%s %.2f" h.Symbol h.Value
+            | i, j when i > 0 && j > 0 ->
+                data.[j - 1, i - 1].ToString("#,##0.00")
+
+            | _, _ -> title
+
+        let n = left.Length + 1
+        let m = top.Length + 1
+        Array2D.init n m genElement
+
+    let public read (broker: Broker) (uri: string): InvestmentInfo [] =
+        let convert (e: EasyInvestTable.Row): InvestmentInfo =
+            { Broker = EasyInvest
+              Qty = e.QUANTIDADE
+              Investment = e.``TIPO DE INVESTIMENTO``
+              Description = e.DESCRIÇÃO
+              DueDate = e.VENCIMENTO
+              AgreedRate = e.``TAXA NEGOCIADA``
+              InitialAmount = e.``VALOR APLICADO``
+              GrossAmount = e.``VALOR BRUTO``
+              NetAmount = e.``VALOR LÍQUIDO`` }
+
+        match broker with
+        | EasyInvest ->
+            let data = EasyInvestTable.Load(uri)
+            Seq.map convert data.Rows |> Array.ofSeq
